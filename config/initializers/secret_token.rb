a3e79ba942b8dca57454f8cb8668b0d34780ab2d
David Wilkins <david@wilkox.org>
2013-08-08 16:06:53 +1000

    Generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booksapp::Application.config.secret_token = '31d1a0f500e0d6f3ec6d311247a4af06a65c39e40b2e9fef32f5a32cc1ee5ece18f39c5d1e6a6f5f7521db4a60f8f130369a48ec631ae1abd39e752b447177a3'
